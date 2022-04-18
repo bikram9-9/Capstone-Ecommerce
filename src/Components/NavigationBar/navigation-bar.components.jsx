47703693807c8504a941c70286d4bc7fcae14383
Bikram Singh <bikramjeet.singh@hotmail.com>
2022-04-18 15:45:40 -0500

    latest

--- src/Components/NavigationBar/navigation-bar.components.jsx
+++ src/Components/NavigationBar/navigation-bar.components.jsx
@@ -1,16 +1,24 @@
 import React, { Fragment } from 'react'
 import "./navigation-bar.styles.scss"
 import {Link} from 'react-router-dom'
+import marsLogo from '../../Assets/mars.png'
+import './navigation-bar.styles.scss'
 
 const Navbar = () => {
   return (
     <Fragment>
-        <Link to="/">
-            Home
-        </Link>
-        <Link to="/signup">
-            Sign Up
-        </Link>
+      <div className='navContainer'>
+        <img src={marsLogo} width='40px'/>
+        <div className='linksContainer'>
+          <Link to="/">
+              Home
+          </Link>
+          <Link to="/signup">
+              Sign Up
+          </Link>
+        </div>
+
+      </div>
     </Fragment>
   )
 }
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import PrimaryButton from '../../Components/buttons/primary-button/primary-button.component';
 import InputField from '../../Components/input-field/input-field.component';
 import './sign-up.styles.scss';
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../firebase/firebase.util';
-import { async } from '@firebase/util';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../firebase/firebase.util';
+import  mars from '../../Assets/mars.png';
 
 export class SignUp extends Component {
 
@@ -52,10 +52,17 @@ export class SignUp extends Component {
         const {user} = await signInWithGooglePopup();
        const userDocRef= await createUserDocumentFromAuth(user);
     }
+    logGoogleRedirectUser = async() =>{
+        const {user} = await signInWithGoogleRedirect();
+    //    const userDocRef= await createUserDocumentFromAuth(user);
+    console.log(user);
+    }
 
     render() {
         return (
             <div className="signUpPage">
+                <img src={mars} alt="MARS Residential Logo" width="100px" />
+                <h1>Sign Up</h1>
                 <div className="formContainer">
                     {this.state.signupInfo.map(input => {
                         return <InputField className="inputContainer" key={input.label} label={input.label} name= {input.name} type= {input.type} placeholder= {input.placeholder}/>
@@ -63,6 +70,9 @@ export class SignUp extends Component {
                     <PrimaryButton  text="Sign Up" redirectTo="/"onClickHandler ={this.onClickHandler}/>
                     <button onClick={this.logGoogleUser}>
                         Sign In with Google
+                    </button>
+                    <button onClick={this.logGoogleRedirectUser}>
+                        Sign In with Google Redirect
                     </button>
                 </div>
             </div>
